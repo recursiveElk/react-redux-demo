df2597f8db3800448ee971d576ed589e8e377540
Ian <finneom@gmail.com>
2017-01-16 13:30:44 +1300

    Initial Commit.

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, {PropTypes} from 'react';
+import {Link} from 'react-router';
+
+class App extends React.Component {
+    render(){
+        return (
+            <div className="container-fluid">
+                <h1>Header?</h1>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+App.propTypes = {
+    children: PropTypes.object.isRequired
+};
+
+export default App;
---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class AboutPage extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>About</h1>
+                <p>This App uses React, Redux, React Router and other libaries.</p>
+                <Link to="\" className="btn btn-primary">Return Home</Link>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
