fec464195ef3107a90f2077d34bbfe611608295e
Ian <finneom@gmail.com>
2017-01-18 09:49:43 +1300

    Manage Course Page template

--- src/components/course/CourseForm.js
+++ src/components/course/CourseForm.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import TextInput from './common/TextInput';
-import SelectInput from './common/SelectInput';
+import TextInput from '../common/TextInput';
+import SelectInput from '../common/SelectInput';
 
 const CourseForm = ({course, allAuthors, onSave, onChange, loading, errors}) => {
   return (
     <form>
-      <h1>Manage Course</h1>
       <TextInput
         name="title"
         label="Title"
@@ -53,3 +52,5 @@ CourseForm.propTypes = {
   loading: React.PropTypes.bool,
   errors: React.PropTypes.object
 };
+
+export default CourseForm;
