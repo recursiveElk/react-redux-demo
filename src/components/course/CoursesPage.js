3d0916d82b22e833aed487244deb9dd49a26cf0f
Ian <finneom@gmail.com>
2017-01-17 15:21:35 +1300

    Update to courses page, Working Watch links.

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -3,46 +3,23 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {Link} from 'react-router';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
-
-      this.state = {
-        course: {title: ''}
-      };
-
-      this.onTitleChange = this.onTitleChange.bind(this);
-      this.onClickSave = this.onClickSave.bind(this);
-  }
-    
-  onClickSave(event) {
-    this.props.actions.createCourse(this.state.course);
-  }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
   }
 
   courseRow(course, index) {
     return <div key={index}>{course.title}</div>;
   }  
+
   render(){
+  const {courses} = this.props;
     return (
     <div>
       <h1>Courses</h1>
-      {this.props.courses.map(this.courseRow)}
-      <h2>Add Course</h2>
-      <input 
-        type="text"
-        onChange={this.onTitleChange}
-        value={this.state.course.title} />
-      <input 
-        type="submit"
-        onClick={this.onClickSave}
-        value="Save" />
+      <CourseList courses={courses}/>
     </div>
     );
   }
